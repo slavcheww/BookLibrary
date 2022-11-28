b2acd0a6d64b0fe286ea8249ba60736c582f30c1
t.slavchev <temelko.slavchev@scalefocus.com>
2022-11-28 12:16:23 +0200

    Add add book functionality.

--- app/src/main/java/com/example/booklibrary/DatabaseHelper.java
+++ app/src/main/java/com/example/booklibrary/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.booklibrary;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -37,5 +39,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_AUTHOR, author);
+        cv.put(COLUMN_PAGES, pages);
+        long result = db.insert(TABLE_NAME, null, cv);
+        
+        if (result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(context, "Added Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
