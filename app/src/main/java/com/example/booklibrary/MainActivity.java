0a0ba1a378c3050595abba021363c398284c11a7
t.slavchev <temelko.slavchev@scalefocus.com>
2022-11-28 13:11:39 +0200

    Add functionality for display databse items.

--- app/src/main/java/com/example/booklibrary/MainActivity.java
+++ app/src/main/java/com/example/booklibrary/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.booklibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+
+    DatabaseHelper dbHelper;
+    ArrayList<String> book_id, book_title, book_author, book_pages;
+
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        dbHelper = new DatabaseHelper(MainActivity.this);
+        book_title = new ArrayList<>();
+        book_author = new ArrayList<>();
+        book_pages = new ArrayList<>();
+        book_id = new ArrayList<>();
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(MainActivity.this, book_id, book_title, book_author, book_pages);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = dbHelper.readAllData();
+        if (cursor.getColumnCount() == 0){
+            Toast.makeText(this, "No data available.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            while(cursor.moveToNext()){
+                book_id.add(cursor.getString(0));
+                book_title.add(cursor.getString(1));
+                book_author.add(cursor.getString(2));
+                book_pages.add(cursor.getString(3));
+            }
+        }
     }
 }
